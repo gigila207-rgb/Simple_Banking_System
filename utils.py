2aef09d92b146677a8e41e9a870e2e6b9a422197
gigila207-rgb <gigila207@gmail.com>
2026-08-03 21:36:25 +0100

    Update header and add menu display function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 
+
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
@@ -12,5 +13,15 @@ def show_header():
     clear_screen()
 
     print("=" * 45)
-    print("          🏦 PYTHON BANK")
+    print("             🏦 PYTHON BANK")
     print("=" * 45)
+
+
+def show_menu():
+    print("\nWelcome to your account!\n")
+
+    print("1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Transaction History")
+    print("5. Exit")
